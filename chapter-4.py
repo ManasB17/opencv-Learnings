b4a69df3af4b8c10c8bbe8074d79a6ce70ae8ce9
ManasB17 <bhattaraimanas@gmail.com>
2022-09-15 20:33:30 +0545

    newcommit

--- chapter-4.py
+++ chapter-4.py
@@ -2,3 +2,17 @@ import cv2
 import numpy as np
 
 img = cv2.imread("Resources/photo5.jpg")
+
+width, height = 460, 460
+pts1 = np.float32([[123, 213], [262, 224], [250, 420], [97, 407]])
+pts2 = np.float32([[0,0], [width, 0], [0, height], [width, height]])
+matrix = cv2.getPerspectiveTransform(pts1, pts2)
+imgOutput = cv2.warpPerspective(img, matrix,(width, height))
+
+
+
+
+# cv2.imshow("Image", img)
+cv2.imshow("output", imgOutput)
+
+cv2.waitKey(0)
